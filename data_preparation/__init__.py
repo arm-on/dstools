a2b463e340bae7f8a1c2169b7f67e6e952c4f70c
Arman Malekzadeh <malekzadeh@ieee.org>
2021-10-25 23:26:38 +0330

    changes made

--- data_preparation/__init__.py
+++
@@ -1,9 +0,0 @@
-def train_test_dev_split(items, train, test, do_shuffle=True):
-    '''
-    Split the data into training, testing, and development sets
-    '''
-    dev = 100-(train+test)
-    from sklearn.model_selection import train_test_split
-    train_items, temp_items = train_test_split(items, test_size=(test+dev)/100, random_state=42)
-    test_items, dev_items = train_test_split(items, test_size=dev/(dev+test), random_state=42)
-    return train_items, test_items, dev_items
--- data_preparation/io.py
+++ data_preparation/io.py
@@ -36,3 +36,28 @@ def load_file(path, format='unknown'):
         return load_txt_line_by_line(path)
     else:
         raise ValueError('format not supported')
+
+def write_file(var, path, format='unknown'):
+    import json
+    import pandas as pd
+    '''
+    Write a variable into a file
+    '''
+    def write_json():
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(var, file)
+    def write_csv():
+        var.to_csv(path)
+    def write_txt_line_by_line():
+        with open(path, 'w', encoding='utf-8') as file:
+            for idx, line in enumerate(var):
+                file.write(line+'\n' if idx != len(var)-1 else line)
+    format = path.split('.')[-1] if format=='unknown' else format
+    if format == 'csv':
+        return write_csv()
+    elif format == 'json':
+        return write_json()
+    elif format == 'txt':
+        return write_txt_line_by_line()
+    else:
+        raise ValueError('format not supported')
