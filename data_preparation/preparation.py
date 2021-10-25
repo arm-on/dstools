ec7911afb4aefd23581dff2c1405c6466444487c
Arman Malekzadeh <malekzadeh@ieee.org>
2021-10-26 01:00:19 +0330

    Update preparation.py

--- data_preparation/preparation.py
+++ data_preparation/preparation.py
@@ -1,4 +1,10 @@
 def train_test_dev_split(items, train, test, dev):
+    from random import shuffle
+    import pandas as pd
+    if isinstance(items, pd.DataFrame):
+        items.sample(frac=1)
+    else:
+        shuffle(items)
     assert train+test+dev==100, "partitions should sum to 100"
     num_items = len(items)
     num_train = int(num_items*train/100)
